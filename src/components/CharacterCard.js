51ec2e6f250b095c054ec5638004d31a9419d6c5
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-26 12:43:21 -0500

    Update CharacterCard.js

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react';
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
-}
+export default function CharacterCard({ character }) {
+  return (
+    <Card>
+      <Image src={character.image} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{character.name}</Card.Header>
+        <Card.Meta>
+          <span className="date">{character.species} {character.status}</span>
+        </Card.Meta>
+        <Card.Description>
+          Location: {character.location.name} <br />
+          Origin: {character.origin.name}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a href={`/episodes`}>
+          <Icon name="user" />
+          Episodes
+        </a>
+      </Card.Content>
+    </Card>
+  );
+};
