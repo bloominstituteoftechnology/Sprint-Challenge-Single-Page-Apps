93c4176a71c21403c6190dc776fe24b233d86d4d
Mimi Hoang <miminhoang1@gmail.com>
2019-07-26 14:00:10 -0400

    struggling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from 'react'
-
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
-}
+export default function CharacterCard ({ name, image, location, origin, species, status }) {
+   return (
+    <div>
+      {name}
+      {image}
+      {location.name}
+      {origin.name}
+      {species}
+    </div>
+  )
+}	
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from 'react';
-
+import axios from 'axios'
+import CharacterCard from './CharacterCard'
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [character, setCharacter] = useState([])
   useEffect(() => {
+    const getCharacter = () => {
+      axios.get('https://rickandmortyapi.com/api/character/')
+      //  Important: verify the 2nd `useEffect` parameter: the dependancies array!	      
+        .then(results => {
+          console.log('results', results.data.results)
+          setCharacters(results.data.results)
+        })
+        
+   
+  
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+  
 
-  return <section className='character-list grid-view'>
+    return <section className='character-list grid-view'>
 
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-
-}
+    {characters.map(character => {
+      	  return <CharacterCard key={character.id} {...character} />;
+   })}
+ </section>	   
+  
+  }
