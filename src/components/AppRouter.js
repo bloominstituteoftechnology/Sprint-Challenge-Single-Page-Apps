b4eba542acaa5db96d6a6970897658d4e8cebd4b
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-26 12:43:18 -0500

    Update AppRouter.js

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,15 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 import CharacterList from './CharacterList'
 import WelcomePage from './WelcomePage';
 import LocationsList from './LocationsList';
+import EpisodesList from './EpisodeList';
 
 export default function AppRouter() {
-  return <div className="page-view ui bottom attached segment active tab">
-    <Switch>
-      <Route path='/characters' component={CharacterList} />
-      <Route path='/locations' component={LocationsList} />
-      <Route component={WelcomePage} />
-      {/* <Route path="/episodes" component={} /> */}
-    </Switch>
-  </div>
-
-}
+  return (
+    <div className="page-view ui bottom attached segment active tab">
+      <Switch>
+        <Route exact path='/' component={WelcomePage} />
+        <Route path="/episodes" component={EpisodesList} />
+        <Route path='/characters' component={CharacterList} />
+        <Route path='/locations' component={LocationsList} />
+      </Switch>
+    </div>
+  );
+};
