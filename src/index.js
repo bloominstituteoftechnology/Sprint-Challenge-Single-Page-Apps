a28a9cb595458c40b1aa5881b41e13059e7986a3
Chris Johnston <cmjohnston@gmail.com>
2019-07-26 11:08:43 -0500

    ???????

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
+import Axios from 'axios';
 
 ReactDOM.render(<Router>
   <App />
