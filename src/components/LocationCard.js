7211586bc4ea596bbe88c3c83f173485705b3fb6
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-26 12:43:30 -0500

    Update LocationCard.js

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'semantic-ui-react';
 
-export default function LocationCard ({ name, type, dimension, residents }) {
-  // image={image}
-  return (<span>todo: location</span>
-  )
-}
+export default function LocationCard({ location /* name, type, dimension, residents */ }) {
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{location.name}</Card.Header>
+        <Card.Description>
+          {location.type} - {location.dimension}
+        </Card.Description>
+        <Card.Meta>Residents: {location.residents.length}</Card.Meta>
+      </Card.Content>
+    </Card>
+  );
+};
