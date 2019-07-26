f408dd58c0e5d44921b6639be3ad8090c8226583
Greg Johnston <GCJohnston91@gmail.com>
2019-07-26 11:39:22 -0400

    Character styling done

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 export default function CharacterCard ({ name, image, location, origin, species, status }) {
 
   return (
-    <div>
-      {name}
-      {image}
-      {location.name}
-      {origin.name}
-      {species}
-    </div>
+    <Card
+      header={name}
+      image={image}
+      description= {`Last seen: ${location.name}`}
+      meta={species}
+      extra = {`From: ${origin.name}`}
+    />
   )
 }
