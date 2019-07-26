55f1bc23ece041733b388ae90737bd77f0a21c8b
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-26 12:43:32 -0500

    Update LocationsList.js

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import Axios from 'axios';
+import LocationCard from './LocationCard';
 
 export default function LocationsList() {
+  const [locations, setLocations] = React.useState([]);
+  React.useEffect(() => {
+    Axios
+      .get("https://rickandmortyapi.com/api/location/")
+      .then(res => {
+        setLocations(res.data.results);
+      })
+      .catch(err => {
+        console.log('%c Error Caught!', 'color: red; font-weight: bold;');
+        console.log(err);
+      });
+  }, []);
 
-}
+  return (
+    <section className="character-list grid-view">
+      {locations.map(location => {
+        return <LocationCard location={location} key={location.url} />;
+      })}
+    </section>
+  );
+};
