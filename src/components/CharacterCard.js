f451b74990031025ad4104ff2e98d9bcbca74af9
johnschneider1 <johnlawschneider@gmail.com>
2019-07-26 11:34:34 -0500

    working my way step by step

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard({ char }) {
+  return (
+    <Card>
+      <Image src={char.image} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{char.name}</Card.Header>
+        <Card.Meta>{char.species}</Card.Meta>
+        <Card.Description>Location: {char.location.name}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          episodes
+        </a>
+      </Card.Content>
+    </Card>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [character, setCharacter] = useState([]);
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+    Axios.get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        console.log("testing the res", res.data.results);
+        setCharacter(res.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+  return (
+    <section className="character-list grid-view">
+      {character.map(char => {
+        return <CharacterCard key={char.id} char={char} />;
+      })}
     </section>
-
+  );
 }
