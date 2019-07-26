a03ce960aa1f312f837c775b74bccf0d41e051c2
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-26 12:43:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import TabNav from './components/TabNav.js';
 import Header from './components/Header.js';
 import AppRouter from './components/AppRouter.js';
 
 export default function App() {
-  return <main>
-    <Header />
-    <TabNav />
-    <AppRouter />
-  </main>
-}
+  return (
+    <main>
+      <Router>
+        <Header />
+        <TabNav />
+        <AppRouter />
+      </Router>
+    </main>
+  );
+};
