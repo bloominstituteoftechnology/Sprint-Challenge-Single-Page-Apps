b9a81e3d20f9d3f078683548f711299c5c4fbf0f
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-07-26 12:17:59 -0400

    fixed typos

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import CharacterList from './CharacterList';
 import WelcomePage from './WelcomePage';
 import LocationsList from './LocationsList';
--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function CharacterCard({ /* add props */ }) {
-  return (<span>todo: location</span>);
+  return (
+    <span>todo: location</span>
+  );
 }
