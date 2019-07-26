b4f4205b407ea4dcb14e74d5f01198211c6524e5
Greg Johnston <GCJohnston91@gmail.com>
2019-07-26 11:31:35 -0400

    All pages rendering data. Reading ReadMe and starting styles

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import LocationCard from './LocationCard'
+import axios from 'axios';
 
-export default function LocationsList() {
+export default function LocationList() {
+
+  const [locations, setLocations] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/location/')
+      .then(results => {
+        console.log('results', results.data.results)
+        setLocations(results.data.results)
+      })
+  }, [])
+
+  return <section className='character-list grid-view'>
+      { locations.map(location => {
+        return <LocationCard key={location.id} {...location} />;
+      })}
+    </section>
 
 }
