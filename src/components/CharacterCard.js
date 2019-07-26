285612b86176cd92c19ac66f416ae4cfd5f13fc0
dangolbeeker <scottb305@outlook.com>
2019-07-26 12:02:13 -0400

    Characters Displaying

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
-}
+export default function CharacterCard (props) {
+  return (
+  <span className='character-card'>
+    <img src={props.img} alt='character'/>
+    <h2 className='character-name'>{props.name}</h2>
+    <p>Status: {props.status}</p>
+    <p>Species: {props.species}</p>
+    <p>ID: {props.number}</p>
+  </span>
+  );
+  }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+export default function CharacterList(props) {
+  const [characters, setCharacters ] = useState([]);
 
-  useEffect(() => {
-    // TODO: Add AJAX/API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
-
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+    useEffect(() => {
+        axios.get(`https://rickandmortyapi.com/api/character/`)
+            .then(res => {
+                console.log('Yahtzee', res.data.results);
+                setCharacters(res.data.results)
+            })
+            .catch(error => {
+                console.error('No Go', error)
+            })
+    },[]);
+  return (
+    <section className='character-list grid-view'>
+     {characters.map(character => (
+              <CharacterCard 
+              img={character.image}
+              species={character.species}
+              name={character.name}
+              status={character.status}
+              number={character.id} />
+            ))}
     </section>
-
-}
+  );
+     }
