f7539b1afe7834f3af87d0e29dc4721085ad469e
Kelly M <moreira.kd@outlook.com>
2019-07-26 16:37:01 -0400

    progess almost there. displaying episode content

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { Card } from 'semantic-ui-react';
 
 
+
+
 export default function LocationCard ({ name, type, dimension, residents }) {
   // image={image}
   return (
 
-    <Card 
-    header={props.name}
-    type={props.type}
-    dimension={props.dimension}
-    residents={props.residents}
-    
-    
-    />
+    <Card>
+
+
+
+
+    </Card>
+
+  
   )
 
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import LocationCard from './LocationCard.js';
+
 
 export default function LocationsList() {
+
+    const [location, setLocation] = useState([]);
+
+    useEffect(() => {
+
+        axios.get('https://rickandmortyapi.com/api/location/')
+        .then(res => {
+            console.log('working', res.data.results);
+
+            setLocation(res.data.results);
+        })
+
+        .catch(err => console.log(err));
+    },[]);
+
+
+    return (
+
+        <div>
+
+{location.map(locations => (
+                <LocationCard
+
+                name={location.name}
+                type={location.type}
+                dimension={location.dimension}
+                residents={location.residents}
+                
+                
+                />
+            ))}
+
+
+        </div>
+
+
+
+    );
 
 }
