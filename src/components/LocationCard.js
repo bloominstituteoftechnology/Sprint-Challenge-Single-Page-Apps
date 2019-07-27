e98e8d31c1bbcb104cd234d4eb630b41c1ab7220
Ben Allen <BenAllen@Vivians-MacBook-Pro-2.local>
2019-07-27 07:49:37 -0500

    Completed LocationCard

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
-export default function LocationCard ({ name, type, dimension, residents }) {
-  // image={image}
-  return (<span>todo: location</span>
+import { Card, Icon } from 'semantic-ui-react'
+
+export default function LocationCard ({ location }) {
+  //  name, type, dimension, residents 
+  return (
+    <Card>
+    <Card.Content header={location.name} />
+    <Card.Content description={location.dimension} />
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        {location.residents.length} Residents
+      </a>
+    </Card.Content>
+  </Card>
   )
 }
