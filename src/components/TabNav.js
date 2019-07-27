0f96b6bd48b2b82629d0b9ac54cac5d758cb4bbd
Ben Allen <BenAllen@Vivians-MacBook-Pro-2.local>
2019-07-27 09:22:52 -0500

    updated TabNav.js

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -16,10 +16,14 @@ const createLabel = (iconName, labelText) => <span><Icon name={iconName} />{labe
 
 const welcomeLabel = createLabel("home", "Home Page")
 const characterLabel = createLabel("users", "Characters")
+const locationLabel = createLabel("map outline", "Locations")
+const episodeLabel = createLabel("video", "Episodes")
 
 const panes = [
   { menuItem: <Menu.Item key='home' as={Nav} to={`/`} content={welcomeLabel} /> },
-  { menuItem: <Menu.Item key='characters' as={Nav} to={`/characters`} content={characterLabel} /> }
+  { menuItem: <Menu.Item key='characters' as={Nav} to={`/characters`} content={characterLabel} /> },
+  { menuItem: <Menu.Item key='locations' as={Nav} to={'/locations'} content={locationLabel} />},
+  { menuItem: <Menu.Item key='episodes' as={Nav} to={'/episodes'} content={episodeLabel} />}
 ]
 
 const TabNav = () => <Tab panes={panes} renderActiveOnly={false} />
