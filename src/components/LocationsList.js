3df1cb3468b0e50dacde111688499619e7732243
Ben Allen <BenAllen@Vivians-MacBook-Pro-2.local>
2019-07-27 07:51:51 -0500

    Completed Location List

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+
+import axios from 'axios'
+
+import LocationCard from './LocationCard'
 
 export default function LocationsList() {
+    const [locationList, setLocationList] = useState(null);
 
+    useEffect(() => {
+        axios
+            .get("https://rickandmortyapi.com/api/location/")
+            .then(response => {
+                setLocationList(response.data.results);
+                console.log(response.data.results);
+            });
+    }, []);
+
+    if (!locationList) {
+        return (
+            <div>
+                <h1>Loading List...please wait</h1>
+            </div>
+        );
+    }
+
+    return (
+        <section className="character-list grid-view">
+            {locationList.map((location, i) => (
+                <LocationCard
+                key={i} 
+                location={location}
+                />
+            ))}
+        </section>
+    );
 }
