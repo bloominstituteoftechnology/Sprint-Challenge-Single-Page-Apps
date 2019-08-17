82b95f8194a8098109d9635f1ea4e58af8ba552e
Carl-Stanley <cds.beml@gmail.com>
2019-08-17 07:45:57 -0700

    Commit

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -5,7 +5,28 @@ import { NavLink } from "react-router-dom";
 const Nav = props => (<NavLink exact {...props} activeClassName="active" /> );
 
 
-// TODO: Add missing tabs below
-export default function TabNav() {
+const LabelMaker = (iconName, labelText) => <span><Icon name={iconName} />{labelText}</span>;
 
-};
+
+const locLabel = LabelMaker("location arrow", "Locations");
+
+const epLabel = LabelMaker("video play", "Episodes");
+
+const welcomeLabel = LabelMaker("home", "Home Page");
+
+const charLabel = LabelMaker("users", "Characters");
+
+const panes = [
+
+    { menuItem: <Menu.Item key='home' as={Nav} to={`/`} content={welcomeLabel} /> },
+  
+    { menuItem: <Menu.Item key='characters' as={Nav} to={`/characters`} content={charLabel} /> },
+  
+    { menuItem: <Menu.Item key='locations' as={Nav} to={`/locations`} content={locLabel} /> },
+  
+    { menuItem: <Menu.Item key='episodes' as={Nav} to={`/episodes`} content={epLabel} /> }
+  ]
+
+const TabNav = () => <Tab panes={panes} renderActiveOnly={false} />;
+
+export default TabNav;
