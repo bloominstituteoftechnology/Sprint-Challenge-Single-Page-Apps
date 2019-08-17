1973aade2b357e5e09e59f06ddd7228cef4f4ed9
Kayla Dailey <kayladailey000@gmail.com>
2019-08-16 22:46:40 -0500

    completed study questions and character api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-import AppRouter from "./components/AppRouter.js";
+// import AppRouter from "./components/AppRouter";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <TabNav />
-      <AppRouter />
+      {/* <TabNav />
+      <AppRouter /> */}
+      
     </main>
   );
 }
+
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+const [character, setCharacter]= useState({});
+
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+    Axios.get('https://rickandmortyapi.com/api/character/')
+    .then(apiObject => {
+      console.log ('apiObject', apiObject);
+      setCharacter(apiObject.results.location)
+    }
+    )
   }, []);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>{character.map}</h2>
     </section>
   );
 }
+
+export default CharacterList;
