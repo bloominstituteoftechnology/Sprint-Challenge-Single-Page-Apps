d24d1b4922a2df871a8419e4699ab27787640f2b
AceIsHuman <menoman1995@yahoo.com>
2019-08-17 12:29:54 -0500

    create tab nav

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -4,5 +4,17 @@ import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
 export default function TabNav() {
-
+    return (
+        <Menu>
+            <Menu.Item>
+                <NavLink to="/">Home</NavLink>
+            </Menu.Item>
+            <Menu.Item>
+                <NavLink to="/characters">Characters</NavLink>
+            </Menu.Item>
+            <Menu.Item>
+                <NavLink to="/locations">Locations</NavLink>
+            </Menu.Item>
+        </Menu>
+    )
 };
