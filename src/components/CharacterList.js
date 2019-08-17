ac3f1fc2e8c0a8c10476a513f4857846723ef8d6
AceIsHuman <menoman1995@yahoo.com>
2019-08-17 12:56:43 -0500

    bug fix in axios call

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -6,18 +6,23 @@ import CharacterCard from './CharacterCard'
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     axios.get('https://rickandmortyapi.com/api/character')
-      .then(res => setCharacters(res.results))
+      .then(res => {
+        setCharacters(res.data.results);
+        setIsLoading(false);
+      })
       .catch(err => console.error(err))
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list grid-view">
-      {characters.map(character => <CharacterCard character={character} />)}
+      {!isLoading ? characters.map(character => <CharacterCard character={character} />) : (<p>Content is loading...</p>)}
     </section>
   );
 }
