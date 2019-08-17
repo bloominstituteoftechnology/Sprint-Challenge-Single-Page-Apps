060853564c7eeab69ef729c49771a79d85a9b17f
Carl-Stanley <cds.beml@gmail.com>
2019-08-17 06:22:47 -0700

    commit

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,53 @@
 import React, { useEffect, useState } from "react";
 
+import Axios from 'axios';
+
+import CharacterCard from './CharacterCard';
+
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  
+  const[cartoonChar,setCartoonChar] = useState();
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+   
+  Axios
+  .get(`https://rickandmortyapi.com/api/character/`) // JSON object here
+  .then( response => {
+
+    setCartoonChar(response.data.results);
+
+  })   
+  .catch( error =>{
+
+    console.log("Another Error?, WT...?", error); // Another Error? 
+
+  });
+
+
   }, []);
 
-  return (
-    <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+  
+  // Check for a valid character
+  if (!cartoonChar) {
+
+    return <div>Loading....</div>
+
+  }
+
+  else {
+
+    return 
+    <section> <h1> 
+      {cartoonChar.map((oneperson) => 
+          <CharacterCard tileData = {oneperson}/> 
+        )}
+    </h1></section>
+
+
+  }
+
+
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
+const Nav = props => (<NavLink exact {...props} activeClassName="active" /> );
+
+
 // TODO: Add missing tabs below
 export default function TabNav() {
 
