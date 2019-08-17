76050c174c8471d052effedde6bae5c4f99ba68a
Chris Cruz <christopherc1331@gmail.com>
2019-08-16 21:45:31 -0700

    Finished part #2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
 import LocationsList from "./components/LocationsList.js";
 import EpisodeList from "./components/EpisodeList.js";
+import WelcomePage from "./components/WelcomePage.js";
 // import AppRouter from "./components/AppRouter.js";
 
 export default function App() {
@@ -11,7 +13,10 @@ export default function App() {
     <main>
       <Header />
       <TabNav />
-      <EpisodeList />
+      <Route path="/" exact component={WelcomePage} />
+      <Route path="/characters/" exact component={CharacterList} />
+      <Route path="/locations/" exact component={LocationsList} />
+      <Route path="/episodes/" exact component={EpisodeList} />
       {/* <AppRouter /> */}
     </main>
   );
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
 export default function TabNav() {
-  return <></>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/characters/">Characters</NavLink>
+        </li>
+        <li>
+          <NavLink to="/locations/">Locations</NavLink>
+        </li>
+        <li>
+          <NavLink to="/episodes/">Episodes</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
