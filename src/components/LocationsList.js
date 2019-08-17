d8920caa5a3fc91b01f274e328836f92ee607c57
AceIsHuman <menoman1995@yahoo.com>
2019-08-17 13:17:37 -0500

    create axios call and map over locations

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,25 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-export default function LocationsList() {}
+import LocationCard from './LocationCard'
+
+export default function LocationsList() {
+  const [locations, setLocations] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get('https://rickandmortyapi.com/api/location')
+      .then(res => {
+        setLocations(res.data.results);
+        setIsLoading(false);    
+      })
+      .catch(err => console.error(err))
+  }, []);
+
+  return (
+    <section className="grid-view">
+      {!isLoading ? locations.map(location => <LocationCard location={location} />) : (<p>Content is loading...</p>)}
+    </section>
+  );
+}
