c3448b0e348b9e53755b06e1e46fd8b17229d212
AceIsHuman <menoman1995@yahoo.com>
2019-08-17 13:18:14 -0500

    create location card

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Card } from 'semantic-ui-react';
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+
+export default function LocationCard(props) {
+  const location = props.location;
+
+  return (
+    <Card 
+      header={location.name}
+      meta={location.type}
+      description={
+        <div>
+          <p>{location.dimension}</p><span>{location.residents.length} residents</span>
+        </div>}
+      />
+  );
 }
