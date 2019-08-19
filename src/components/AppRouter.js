13d7ad72006c3ff1bf246dbdf0e5554b50c72205
unknown <toosdai52Otte@gmail.com>
2019-08-18 21:09:42 -0500

    added WelcomePage to Router

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import WelcomePage from './WelcomePage';
 
 function AppRouter(){
     return(
-        <div></div>
+        <Route path="/" component={WelcomePage} />
     );
 }
 
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,11 +1,3 @@
-// import React from "react";
-// import { Tab, Menu, Icon } from "semantic-ui-react";
-// import { NavLink } from "react-router-dom";
-
-// // TODO: Add missing tabs below
-// export default function TabNav() {
-
-// };
 import React, { Component } from 'react'
 import { Icon, Menu } from 'semantic-ui-react'
 import { NavLink } from 'react-router-dom';
