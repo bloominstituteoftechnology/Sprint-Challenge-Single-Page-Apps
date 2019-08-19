78f15e97bc380264522178f24316c73e4d6e52c1
unknown <toosdai52Otte@gmail.com>
2019-08-18 20:32:54 -0500

    added AppRouter file and added material-ui to TabNav

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,8 +1,58 @@
-import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+// import React from "react";
+// import { Tab, Menu, Icon } from "semantic-ui-react";
+// import { NavLink } from "react-router-dom";
 
-// TODO: Add missing tabs below
-export default function TabNav() {
+// // TODO: Add missing tabs below
+// export default function TabNav() {
 
-};
+// };
+import React, { Component } from 'react'
+import { Icon, Menu } from 'semantic-ui-react'
+import { NavLink } from 'react-router-dom';
+
+export default class TabNav extends Component {
+  state = { activeItem: 'homepage' }
+
+  handleItemClick = ({ name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu icon='labeled'>
+        <Menu.Item name='homepage' active={activeItem === 'homepage'} onClick={this.handleItemClick}>
+          <Icon name='home' />
+          <NavLink to="/">HomePage</NavLink>
+        </Menu.Item>
+
+        <Menu.Item
+          name='characters'
+          active={activeItem === 'characters'}
+          onClick={this.handleItemClick}
+        >
+          <Icon name='users' />
+          <NavLink to="/characters">Characters</NavLink>
+        </Menu.Item>
+
+        <Menu.Item
+          name='locations'
+          active={activeItem === 'locations'}
+          onClick={this.handleItemClick}
+        >
+          <Icon name='map outline' />
+            <NavLink to="/locations">Locations</NavLink>
+        </Menu.Item>
+
+        <Menu.Item
+            name="episodes"
+            active={activeItem === "episodes"}
+            onClick={this.handleItemClick}
+        >
+            <Icon name="video camera" />
+            <NavLink to="episodes">Episodes</NavLink>
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
+
