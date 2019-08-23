cafb4fc80e5a5d7ea0384b26c9c02d7851937b87
Kelly M <moreira.kd@outlook.com>
2019-08-23 13:12:59 -0400

    tabnav

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <main>
       <Header />
-      {/* <TabNav /> */}
+      <TabNav />
       <CharacterList />
       <LocationsList />
       <EpisodeList />
