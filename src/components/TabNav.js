ce0bc879006a810a8fe113f993966db85bb734fd
Christina <chmagorton@gmail.com>
2019-08-22 21:13:49 -0600

    Adding extra notes about Semantic UI

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -3,6 +3,9 @@ import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
+// Take a look at React Semantic UI tabs 
+// https://react.semantic-ui.com/modules/tab/
 export default function TabNav() {
 
 };
+
