d7f0724f5e43ff64b38f1dd35bf178b381fa4365
Christina <chmagorton@gmail.com>
2019-08-22 21:12:52 -0600

    Taking AppRouter out of App.js to avoid errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-import AppRouter from "./components/AppRouter.js";
+
 
 export default function App() {
   return (
     <main>
       <Header />
-      <TabNav />
-      <AppRouter />
+      {/* <TabNav /> */}
     </main>
   );
 }
