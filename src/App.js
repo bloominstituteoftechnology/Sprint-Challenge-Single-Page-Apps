85a2a70596c9ee83216d14382f6db6a5941d908b
juarezfrench <markedfrench@gmail.com>
2019-08-25 15:09:37 -0500

    Initial work on App page - Axios call set up.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import WelcomePage from './components/WelcomePage';
 
 
 export default function App() {
