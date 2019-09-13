b1fae53b72a533be4fa55ffc366e9406102f9350
spettigrew <sara.pettigrew5@gmail.com>
2019-09-13 17:29:56 -0600

    "answers to free-form  questions"

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from "react";
 
-export default function CharacterList() {
+export default function CharacterList(props) => {
+  const [characterList, setCharacterList] = useState ([])
   // TODO: Add useState to track data from useEffect
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+        // TODO: Add API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependencies array!
+    const getCharacterList = () => {
+      axios
+        .get(`https://rickandmortyapi.com/api/character/${name}`)
+        .then(response => {
+          setCharacterList(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+
+    getMovies();
+  }, [name]);
+
 
   return (
     <section className="character-list grid-view">
