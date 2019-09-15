b0a489c56f313cc65ff08031fe6bbbe05e3c5f7d
Andrew Nottoli <nottolivc@gmail.com>
2019-09-15 16:33:32 -0700

    structured the App.js file logic, added semantic ui, route, dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-import TabNav from "./components/TabNav.js";
+import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
+import WelcomePage from './components/WelcomePage.js';
+import TabNav from "./components/TabNav.js";
+import CharacterList from './components/CharacterList.js';
+import LocationsList from './components/LocationsList.js';
+import EpisodeList from './components/EpisodeList.js';
 
+/* whole page to be rendered using semantic ui per github documentation, added import of all components to be rendered
+added import of route to route paths for all components */
 
 export default function App() {
   return (
     <main>
-      <Header />
-      {/* <TabNav /> */}
+      <Header></Header>
+      <TabNav></TabNav>
+      <Route exact path='/' component={WelcomePage} />
+      <Route path='/characters' component={CharacterList} />
+      <Route path='/locations' component={LocationsList} />
+      <Route path='/episodes' component={EpisodeList} />
     </main>
   );
-}
+};
