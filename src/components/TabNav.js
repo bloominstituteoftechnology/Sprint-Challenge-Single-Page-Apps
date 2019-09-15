ad7e2e0ea95beb8144e4292a93d15af56655caad
Andrew Nottoli <nottolivc@gmail.com>
2019-09-15 16:40:32 -0700

    created components tabnav, header seems fine, added charcterlist, locationslist, episode listto logic of tabnav and semantic ui rendering

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,15 +1,32 @@
 import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
+import { Tab, Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
-// TODO: Add missing menu/tabs/nav below
 
-// Review Semantic UI Component options for nav-like UI:
-// https://react.semantic-ui.com/collections/menu/
+
 // https://react.semantic-ui.com/modules/tab/
-// https://react.semantic-ui.com/elements/button/
-// https://react.semantic-ui.com/collections/breadcrumb/
+/* Semantic UI Icons and Pane menu item array render for Tab function */
+const panes = [
+    {
+     menuItem: { key: 'homepage', icon: 'home', content: <NavLink exact to='/'>Homepage</NavLink> },
+    },
+    {
+     menuItem: { key: 'characters', icon: 'arrow circle down', content: <NavLink to='/characters'>Characters</NavLink> },
+    },
+    {
+     menuItem: { key: 'locations', icon: 'arrow circle down', content: <NavLink to='/locations'>Locations</NavLink> },
+    },
+    {
+      menuItem: { key: 'episodes', icon: 'file video', content: <NavLink to='/episodes'>Episodes</NavLink> },
+    }
+  ];
 
+/* render return semantic ui tab array of menu items via panes in Tab function */
 export default function TabNav() {
-
+    return( 
+        <Menu>
+            <Tab panes={panes} />
+        </Menu>
+    );
 };
+
