a1bfe3e658667a92e93589dbd8f0f3f2b15bc672
Andrew Nottoli <nottolivc@gmail.com>
2019-09-15 16:48:25 -0700

    added character card and state to character list component, and API request, console, shows object passed back from api, map chars from state to render grid view of chars

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { Card, Image } from 'semantic-ui-react'
+export default function CharacterCard(props) {
+/* rendering UI using built in semantic ui card grid design and images via props */
+  return(
+  <Card>
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+    <Card.Content>
+    <Card.Header>{props.name}</Card.Header>
+
+    <Image src={props.image} wrapped ui={true} />
+    
+    <Card.Meta>
+      <span>  {props.species} </span> 
+      <span>  {props.status}  </span>
+    </Card.Meta>
+    
+    <Card.Description>
+      <span>Location: {props.location}</span>
+      <span>Origin: {props.origin}</span>
+    </Card.Description>
+    
+    </Card.Content>
+  </Card>
+  );
+};
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from "react";
-
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
+/* rendering UI using semantic card and icons */
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-
+  const [chars, setChars] = useState([]);
+/* call the chars and set the state of setChars to the axios get api request */
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character')
+    .then(response => {
+      console.log(response.data.results);
+      setChars(response.data.results);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }, []);
 
+/* return div with mapped characters from API data, rendered to client route page through character card function */
   return (
-    <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <div className="character-list grid-view">
+      
+    {chars.map(character => 
+        <CharacterCard 
+          image={character.image}
+          name={character.name} 
+          species={character.species} 
+          location={character.location.name} 
+          origin={character.origin.name}
+          key={character.id} 
+        />
+        )}
+  </div>
   );
-}
+};
