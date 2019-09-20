fd683da73335d353328e5f07ff70d339673ae3bc
Greg <gsc229@gmail.com>
2019-09-20 13:58:24 -0400

    search bar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchForm from "./SearchForm";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <SearchForm />
       <Link to="/">Welcom Page</Link>
       <Link to="/character-list">Characters</Link>
     </header>
