b85e9d1545a4ca2f3830fd231111b34d8cdbfc61
meera-andersen <meera.andersen@gmail.com>
2019-09-20 10:14:21 -0600

    created search input field and rendered

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-import Header from "./components/Header.js";
+import Header from "./components/Header.js"
 import WelcomePage from "./components/WelcomePage.js"
-import CharacterList from "./components/CharacterList.js";
+import CharacterList from "./components/CharacterList.js"
+import SearchForm from "./components/SearchForm.js"
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <SearchForm/>
       <CharacterList/>
     </main>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,8 +3,15 @@ import React, { useState } from "react";
 export default function SearchForm() {
  
   return (
-    <section className="search-form">
-     // Add a search form here
+    <section className ="search-form">
+      <form>
+        <input 
+        type = "text" 
+        name = "search"
+        placeholder = "Search for characters by name"
+        />
+        <button>Search</button>
+      </form> 
     </section>
   );
 }
