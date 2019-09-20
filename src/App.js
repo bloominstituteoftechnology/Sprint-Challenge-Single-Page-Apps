0b038aacfea26ed3eff51cddd38088d316d82c4c
juarezfrench <markedfrench@gmail.com>
2019-09-20 12:46:19 -0500

    copied card template to location, data is there, but weirdly formatted. I will deal with that later...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header.js";
 import TabNav from "./components/TabNav.js";
 import WelcomePage from './components/WelcomePage';
 import CharacterList from './components/CharacterList';
-// import LocationsList from './components/LocationsList';
+import LocationsList from './components/LocationsList';
 // import EpisodeList from './components/EpisodeList';
 // import SearchForm from './components/SearchForm';
 
@@ -16,8 +16,8 @@ export default function App() {
 
       <Route exact path='/' component={WelcomePage} />
       <Route path='/character' component={CharacterList} />
-      {/* <Route path='/location' component={LocationsList} />
-      <Route path='/episode' component={EpisodeList} /> */}
+      <Route path='/location' component={LocationsList} />
+      {/* <Route path='/episode' component={EpisodeList} /> */} */}
        {/* <Route path='/search' component={SearchForm} /> */}
 
     </main>
