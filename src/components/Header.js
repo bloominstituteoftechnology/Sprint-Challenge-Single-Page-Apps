d7c8b3d3422764ea44d33ac98f30c0f1c24d65d7
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-09-20 14:02:34 -0400

    Search Form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CharacterList from "./CharacterList";
-export default function Header() {
+import SearchForm from "./SearchForm";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+export default function Header(props) {
+  const [character, setCharacter] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const search = searchValue => {
+    setLoading(true);
+    axios
+      .get("https://rickandmortyapi.com/api/character/?name={searchValue}")
+      .then(res => {
+        if (res.data.results === "True") {
+          const person = res.data.results;
+          setCharacter(person.search);
+        }
+      })
+      .catch(err => {
+        console.log("The Data was not returned", err);
+      });
+  };
   return (
     <div>
       <header className="ui centered">
         <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
       </header>
+      <SearchForm search={search}/>
       <CharacterList />
     </div>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,31 @@
 import React, { useState } from "react";
+const SearchForm = props => {
+  // I'm passing the current state as the value as empty String for the search input field.
+  const [searchInput, setSearchInput] = useState("");
 
-export default function SearchForm() {
- 
+  //When the onChange event is called, the handleSearch function is called will update the state with the new value.
+  const handleSearch = e => {
+    setSearchInput(e.target.value);
+  };
+
+  //The resetInput function called the state update function (setSearchInput) with an empty string in order to clear the input field.
+  const resetInput = () => {
+    setSearchInput("");
+  };
+  //The callSearchFunction function prevent the default behavior of an input search.
+  const callSearchFunction = e => {
+    e.preventDefault();
+    props.search(searchInput);
+    resetInput();
+  };
   return (
     <section className="search-form">
-     // Add a search form here
+      <form className="search">
+        <input value={searchInput} onChange={handleSearch} type="text" />
+        <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+      </form>
     </section>
   );
-}
+};
+
+export default SearchForm;
