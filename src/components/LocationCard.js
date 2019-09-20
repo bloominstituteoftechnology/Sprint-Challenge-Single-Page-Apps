c172e01671e74f07927827797f7ff4df0990ccd0
juarezfrench <markedfrench@gmail.com>
2019-09-20 17:44:47 -0500

    resent the src and root files after changes.

---
+++ src/components/LocationCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Card, Label } from 'semantic-ui-react';
+
+ const LocationCard = ({ location }) =>{
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{location.name}</Card.Header>
+        <Card.Description>{location.type} - {location.dimension}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Label attached='bottom right'>{location.residents.length} residents</Label>
+      </Card.Content>
+    </Card>
+  );
+}
+
+export default LocationCard
