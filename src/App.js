b17794f9fcfe6c9590c914908fe26524cf41fe9f
juarezfrench <markedfrench@gmail.com>
2019-09-20 13:18:57 -0500

    Everything set up and working except for search. Deleted episode functionality - it wasn't in the MVP. Should have read the MVP. Ugh.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import TabNav from "./components/TabNav.js";
 import WelcomePage from './components/WelcomePage';
 import CharacterList from './components/CharacterList';
 import LocationsList from './components/LocationsList';
-// import EpisodeList from './components/EpisodeList';
-// import SearchForm from './components/SearchForm';
+import SearchForm from './components/SearchForm';
 
 export default function App() {
   return (
@@ -17,8 +16,7 @@ export default function App() {
       <Route exact path='/' component={WelcomePage} />
       <Route path='/character' component={CharacterList} />
       <Route path='/location' component={LocationsList} />
-      {/* <Route path='/episode' component={EpisodeList} /> */} */}
-       {/* <Route path='/search' component={SearchForm} /> */}
+      <Route path='/search' component={SearchForm} />
 
     </main>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
+import {icon} from 'semantic-ui'
 
-export default function SearchForm() {
+const SearchForm =(props) => {
  
   return (
     <section className="search-form">
-     // Add a search form here
+   <div class="ui search">
+  <div class="ui icon input">
+    <input class="prompt" type="text" placeholder="Find a character..."/>
+    <i class="search icon"></i>
+  </div>
+  <div class="results"></div>
+</div>
     </section>
   );
 }
+export default SearchForm
+
