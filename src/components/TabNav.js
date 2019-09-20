339a5d22f38c664175460ab95ba816f516c17071
juarezfrench <markedfrench@gmail.com>
2019-09-20 17:46:57 -0500

    resent the src and root files after changes.

---
+++ src/components/TabNav.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Icon } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
+
+
+export default function Nav() {
+  return (
+    <div className='ui attached tabular menu'>
+      <NavLink exact to='/' className='item'>
+        <Icon name='home' />
+        Home Page
+      </NavLink>
+      <NavLink to='/character' className='item'>
+        <Icon name='users' />
+        Characters
+      </NavLink>
+      <NavLink to='/location' className='item'>
+        <Icon name='map outline' />
+        Locations
+      </NavLink>
+      {/* <NavLink to='/episode' className='item'>
+        <Icon name='video' />
+        Episodes
+      </NavLink> */}
+      {/* <NavLink to='/search' className='item'>
+        <Icon name='search' />
+        Character Search
+      </NavLink> */}
+    </div>
+  )
+};
