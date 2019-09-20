09ff6903548a34344fa663ef8b133dad3fb0b417
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-09-20 08:25:13 -0700

    Added Dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
 
+import CharacterList from "./components/CharacterList";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={CharacterList} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState({})
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const id = props.id;
+    // console.log('This is the ID:', id);
+
+    Axios.get(`https://rickandmortyapi.com/api/character/`${id})
+          .then(response => {
+            setCharacters(response.data)
+            // console.log(response);
+          })
+          .catch(error =>{
+            // console.log(response);
+          })
   }, []);
 
   return (
