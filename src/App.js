95e2ed879819fbaf8e06095fe5f04aa8132e9d0e
juarezfrench <markedfrench@gmail.com>
2019-09-20 11:11:03 -0500

    Set up scaffold, built axios call - data flowing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
+import axios from 'axios' 
 
+ const App= ()=> {
+	const [characters, setCharacters] = useState(null);
 
-export default function App() {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+	useEffect(() => {
+		axios
+			.get("https://rickandmortyapi.com/api/character")
+
+			.then(response => {
+			
+				setCharacters(response);
+				console.log('App.js -> %cresponse:', 'color: limeGreen', response)
+
+				
+			})
+
+			.catch(error => {
+				console.error("Data's not coming through bro, check this out.", error);
+			});
+	}, []);
+
+	return (
+		<main>
+			<Header />
+		</main>
+	);
 }
+export default App
