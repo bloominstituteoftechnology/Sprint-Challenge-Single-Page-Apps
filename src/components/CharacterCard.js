361392f8835e5b31ac0cbeb961bfd41f2de5638f
Greg <gsc229@gmail.com>
2019-09-20 13:34:56 -0400

    Styled the character cards with styled-components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,9 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+const Card = styled.div`
+  background: rgba(0, 0, 0, 0.4);
+  border: 3px solid black;
+  color: white;
+  margin: 20px auto;
+  text-align: center;
+`;
 
 export default function CharacterCard(props) {
   const { name, status, species, image } = props.character;
   return (
-    <div className="character-card">
+    <Card className="character-card">
       <h2>{name}</h2>
       <div className="character-photo">
         <img src={image} alt="" />
@@ -15,6 +23,6 @@ export default function CharacterCard(props) {
         Species: <em>{species}</em>
       </div>
       <span>todo: character</span>
-    </div>
+    </Card>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import styled from "styled-components";
 export default function WelcomePage(props) {
   const routeToCharacters = event => {
     event.preventDefault();
