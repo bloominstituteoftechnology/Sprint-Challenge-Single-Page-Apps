8696e0a6129d50b925567bb2c1b36ffafec78a3e
Jake <panamajake86@gmail.com>
2019-10-11 20:09:33 -0600

    got a bunch of shit to appear.  Not sure what to do next.  Just keep copying and pasting, I guess.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {Route} from "react-router-dom";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+// import SearchForm from "./components/SearchForm";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      {/* <SearchForm /> */}
+      <WelcomePage />
+      <Route exact path="/" component={CharacterList} />
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+const CharacterCard = char => {
+
+  const { name, status, species, image, id } = char;
+
+  return (
+    <div className="wrapper">
+      <div className="char-card">
+        <Link to={`/results/${id}`}>
+          <h2>{name}</h2>
+        </Link>
+        <div className="image">
+          <img src={image} />
+        </div>
+        <div className="species">
+          Species: <strong>{species}</strong>
+        </div>
+        <div className="status">
+          Status: <em>{status}</em>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterCard;
