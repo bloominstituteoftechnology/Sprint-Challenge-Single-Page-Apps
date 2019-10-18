1d54d124d8adbd0c2e43a4315420c72997ae5538
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-18 08:27:35 -0700

    Import Route to App.js.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
 import CharacterList from "./components/CharacterList";
 import Welcome from "./components/WelcomePage";
 
