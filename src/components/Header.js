0e4a5b37b8a70358d6110141e3303c871dc45df5
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-18 08:42:57 -0700

    Style Header file.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,54 @@ import React from "react";
 import { Link} from "react-router-dom";
 import styled from "styled-components";
 
+const Box = styled.div`
+  width: 95%;
+ display: flex;
+ justify-content: space-between;
+
+ h1 {
+   font-size: 2rem;
+ }
+
+ .linkBox {
+   width: 30%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  text-align: center;
+
+    a{
+      text-decoration: none;
+      font-size: 1.8rem;
+      padding: 1%;
+      width: 45%;
+      border-radius: 8px;
+      color:yellow;
+      background-color: green;
+
+        &:hover {
+          color:green;
+          background-color: yellow;
+        }
+    }
+ }
+ 
+ 
+`;
+
 export default function Header() {
   return (
     <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-      <Link to="/">Home</Link>
-      <Link to="/character-list">Characters</Link>
+
+       <Box>
+        <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+        <div className="linkBox">
+          <Link to="/">Home</Link>
+          <Link to="/character-list">Characters</Link>
+        </div>
+       
+      </Box>
     </header>
   );
 }
+
