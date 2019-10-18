f6297f67a87777feae3936a7b2f82d0c5be4ec0f
itsericfig <itsericfig@gmail.com>
2019-10-18 10:48:11 -0500

    Character Cards with character data from api is rendered onto the screen

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ character }) {
+    const { image, name, species, gender, status, origin, episode } = character;
+    return (
+        <div className="movie-card">
+            <img src={image} />
+            <h2>{name}</h2>
+            <p>Species: {species}</p>
+            <p>Gender: {gender}</p>
+            <p>Status: {status}</p>
+            <p>Origin: {origin.name}</p>
+            <p>Featured in {episode.length} episode/s</p>
+        </div>
+    );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
     // TODO: Add useState to track data from useEffect
@@ -23,7 +24,9 @@ export default function CharacterList() {
 
     return (
         <section className="character-list">
-            <h2>TODO: `array.map()` over your state here!</h2>
+            {characters.map(character => (
+                <CharacterCard key={character.id} character={character} />
+            ))}
         </section>
     );
 }
