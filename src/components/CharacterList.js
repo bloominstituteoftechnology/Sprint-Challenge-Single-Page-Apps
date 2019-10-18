e4d50fbe692736e17cd82ed6e4a3f394b55b9c2e
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-18 08:58:29 -0700

    Add Card Page Change buttno. Style button.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -10,15 +10,31 @@ Justify-content: center;
 flex-wrap: wrap;
 `;
 
+const Button = styled.button`
+
+width: 10%;
+font-size: 1.3rem;
+color: orange;
+background: black;
+border-radius: 8px;
+border: 2px solid black;
+margin: 5px;
+&:hover{
+  color: black;
+background: orange;
+}
+`;
+
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [info, setInfo] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array! 
     axios
-        .get(`https://rickandmortyapi.com/api/character/`)
+        .get(`https://rickandmortyapi.com/api/character/?page=${page}`)
         .then(response => {
           console.log("From CharacterCard:" , response.data.results)
           const characterData = response.data.results
@@ -29,10 +45,15 @@ export default function CharacterList() {
           console.log("The data was not returned", error);
         });
     
-  }, []);
+  }, [page]);
 
   return (
     <section className="character-list">
+      <Button onClick={() => 
+          (page > 2) ? setPage(page - 1): setPage(1)}
+          >Previous</Button>
+
+        <Button onClick={() => setPage(page + 1)}>Next</Button>
       <Grid>
       {info.map((item, id) => {
             return (
