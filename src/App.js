0239a2416c1252870a268c8e5a158927ba614d4c
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-18 08:23:47 -0700

    Removed Home component and passed in WelcomePage component instead. Starting to setup state for Characters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
-import Home from "./components/Home";
 import CharacterCard from "./components/CharacterCard.js";
+import WelcomePage from "./components/WelcomePage.js";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={WelcomePage} />
       <Route exact path="/characters" component={CharacterCard} />
     </main>
   );
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characterData, setCharacterData] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
