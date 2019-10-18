bcaef86da5124728155bd523be47854fb217f613
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-18 08:19:19 -0700

    import axios to CharacterList.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <CharacterList />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Character from './CharacterCard';
 
 export default function CharacterList() {
