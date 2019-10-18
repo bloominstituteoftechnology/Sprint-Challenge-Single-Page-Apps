a146b34b795fed6c5aa87a5659641db9858b541b
itsericfig <itsericfig@gmail.com>
2019-10-18 10:17:33 -0500

    Successfully made api request and displayed an array of rick and morty characters in the console

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+    // TODO: Add useState to track data from useEffect
+    const [characters, setCharacters] = useState([]);
+    useEffect(() => {
+        // TODO: Add API Request here - must run in `useEffect`
+        //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+        const getCharacters = () => {
+            axios
+                .get("https://rickandmortyapi.com/api/character/")
+                .then(response => {
+                    console.log(response.data.results);
+                    setCharacters(response.data.results);
+                })
+                .catch(error => {
+                    console.error("Server Error", error);
+                });
+        };
+        getCharacters();
+    }, []);
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
-
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+    return (
+        <section className="character-list">
+            <h2>TODO: `array.map()` over your state here!</h2>
+        </section>
+    );
 }
