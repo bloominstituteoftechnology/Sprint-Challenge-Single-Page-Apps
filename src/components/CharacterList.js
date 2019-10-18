c04804b501757ef613b54602e2088028d38d2fee
itsericfig <itsericfig@gmail.com>
2019-10-18 12:54:54 -0500

    Styled components using styled-components, implemented search feature...shows cearched person in console.log only

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,7 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const List = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+    max-width: 960px;
+    text-align: center;
+`;
 
 export default function CharacterList() {
     // TODO: Add useState to track data from useEffect
@@ -13,7 +30,7 @@ export default function CharacterList() {
             axios
                 .get("https://rickandmortyapi.com/api/character/")
                 .then(response => {
-                    console.log(response.data.results);
+                    // console.log(response.data.results);
                     setCharacters(response.data.results);
                 })
                 .catch(error => {
@@ -24,12 +41,15 @@ export default function CharacterList() {
     }, []);
 
     return (
-        <section className="character-list">
-            <h2>View all the Rick and Morty Characters!</h2>
+        <Container>
             <Link to="/">Back to Home page</Link>
-            {characters.map(character => (
-                <CharacterCard key={character.id} character={character} />
-            ))}
-        </section>
+            <h2>View all the Rick and Morty Characters!</h2>
+            <SearchForm characters={characters} />
+            <List>
+                {characters.map(character => (
+                    <CharacterCard key={character.id} character={character} />
+                ))}
+            </List>
+        </Container>
     );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+    max-width: 960px;
+    text-align: center;
+`;
 
 export default function WelcomePage() {
     return (
-        <section className="welcome-page">
+        <Container className="welcome-page">
             <header>
                 <h2>Welcome to the ultimate fan site!</h2>
                 <img className="main-img" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="rick" />
+                <p>View the characters below!</p>
                 <Link to="/characterList">Character List</Link>
             </header>
-        </section>
+        </Container>
     );
 }
