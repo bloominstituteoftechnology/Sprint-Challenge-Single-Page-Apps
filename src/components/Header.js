b9db974430663bc83eb7e6da98f64f9fc0764144
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-10-18 14:08:41 -0500

    styles added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+   
     </header>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,33 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+
+
+const SearchStyles = styled.section`
+
+display:flex;
+justify-content:center;`
+
+
 
 export default function SearchForm() {
+  
+  const [search, setSearch] = useState("");
  
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <SearchStyles className="search-form">
+      <div>
+          <label htmlFor="search">
+           
+            <input type="text" id="search" value={search} placeholder ="Search"
+            onChange={event => setSearch (event.target.value)}
+            />
+          </label>
+          <button className="search-button">Submit</button>
+      </div>
+    </SearchStyles>
   );
 }
+
+
+
