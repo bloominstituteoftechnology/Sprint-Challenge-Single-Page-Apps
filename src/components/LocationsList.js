6c917f4411adbc1c5e967e2378a746ee337dd77b
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-18 10:47:58 -0700

    Change home image.

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import LocationCard from "./LocationCard";
 import styled from "styled-components";
-import bgrnd from "../img/bgrnd.png";
+
 
 
 const Input =styled.input`
