4a24c5ba55f5f3a6d0e88c5c1e7189ab7f6b8cb3
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-18 08:20:38 -0700

    Added home component and set up Routers to navigate between home and characters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
-
+import Home from "./components/Home";
+import CharacterCard from "./components/CharacterCard.js";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/characters" component={CharacterCard} />
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/characters">Characters</Link>
+      </nav>
     </header>
   );
 }
