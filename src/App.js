5de1c4e3772c8259bb1112a79d3397e85c7d3f56
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-18 08:26:36 -0700

    Import Welcome page to app.js. Add Welcome page to App function.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
+import Welcome from "./components/WelcomePage";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Welcome />
       <CharacterList />
     </main>
   );
