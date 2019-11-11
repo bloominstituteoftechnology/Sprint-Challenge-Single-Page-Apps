19546114c4c75b4a7e36d1b11f203a2e0e6a62f6
Samantha-Reis <Samantha-Reis@users.noreply.github.com>
2019-11-10 21:50:49 -0500

    Done

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+const CharacterCard = char => {
+
+  const { name, status, species, image, id } = char.character;
+
+  return (
+    <div className="wrapper">
+      <div className="char-card">
+        <Link to={`/results/${id}`}>
+          <h2>{name}</h2>
+        </Link>
+        <Link to="/list">
+        <div className="image">
+          <img src={image} alt={`Photo of ${name}`} />
+        </div>
+        </Link>
+        <div className="species">
+          Species: <strong>{species}</strong>
+        </div>
+        <div className="status">
+          Status: <em>{status}</em>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterCard;
