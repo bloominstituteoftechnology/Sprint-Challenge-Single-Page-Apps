5ce6a0c911c85c51065d4b85a01d006abee16218
brianetaveras <taveras574@gmail.com>
2019-11-10 21:07:18 -0500

    Added WelcomePage to the '/' route path. Added information to character list from the API. Added CharacterCard to the character list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import {Route, Link} from "react-router-dom"
+import WelcomePage from './components/WelcomePage'
+import CharacterList from './components/CharacterList'
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
  
+  const [results, setResults] = useState()
+
+  const handleChanges = e=>{
+    setResults(e.target.value)
+    console.log(results)
+  }
+
+  const submitHandler = e =>{
+    e.preventDefault()
+    const filtered = props.characters.find(el=> el.name.includes(results))
+    props.search([filtered])
+    console.log(filtered)
+  }
+
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form onSubmit={submitHandler}>
+        <input
+          onChange={handleChanges} 
+          type="text"
+          name="character"
+          id="character"
+          placeholder="Search">
+          </input>
+
+      </form>
     </section>
   );
 }
