32f25c9c6c972ca5bb39f16439315f330c8d370f
Edwin Chajon <junchajo@gmail.com>
2019-11-15 10:21:54 -0800

    axios/api, Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage"
+import CharacterList from "./components/CharacterList"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css';
 
 
 export default function App() {
   return (
     <main>
-      <Header />
+      <Header/>
+      <div className="nav-links">
+          <Link className="some-link" to="/">
+            Home
+          </Link>
+          <Link to="/CharacterList">Characters</Link>
+        </div>
+        <Route  exact path="/" component={WelcomePage} />
+        <Route exact path="/CharacterList" component={CharacterList} /> 
+
+
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,49 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import {
+  Container,
+  Card,
+  CardBody,
+  CardTitle,
+  CardImg,
+  CardSubtitle
+} from 'reactstrap';
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [character, setCharacter] = useState([])
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+    axios.get("https://rickandmortyapi.com/api/character/")
+    .then(response => {
+      console.log(response.data.results)
+      setCharacter(response.data.results)
+    })
+    .catch(error => {
+      console.log('The data was not returned', error);
+    });
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <Container>
+        {character.map((char, index) => (
+         <div>
+           <Card>
+           <CardImg top width="50%" src={char.image} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>{char.name}</CardTitle>
+            <CardSubtitle>Status: {char.status}</CardSubtitle>
+            <CardSubtitle>Species: {char.species}</CardSubtitle>
+            <CardSubtitle>Gender: {char.gender}</CardSubtitle>
+            <CardSubtitle>Origin: {char.origin.name}</CardSubtitle>
+          </CardBody>
+          </Card>
+          </div> 
+          ))}
+      </Container>
     </section>
   );
 }
