cd29f7a60a7619f3f1bdc159bf42780d0b900633
skye <skyesajous@gmail.com>
2019-11-15 09:18:04 -0800

    setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header.js";
 
-
 export default function App() {
   return (
     <main>
       <Header />
     </main>
   );
+}
+{
+  setup;
 }
