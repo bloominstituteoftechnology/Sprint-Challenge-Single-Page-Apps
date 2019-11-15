87ae4f1523ae5c26c661ed27125f34803c6dba4e
Jessica Novak <jessnovak2@gmail.com>
2019-11-15 10:46:32 -0800

    mvp

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,40 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Axios from "axios";
+import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [characters, setCharacters] = useState([]);
+  const [filteredData, updateData] = useState([])
+
+ 
+
+  const search = charArr => {
+      updateData(charArr)
+      
+  };
+
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios.get("https://rickandmortyapi.com/api/character/").then(response => {
+      console.log(response.data.results);
+      setCharacters(response.data.results);
+      updateData(response.data.results);
+    });
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>Character List</h2>
+      <Link className="main-buttons" to={"/"}>
+        Home
+      </Link>
+      <SearchForm search={search} characters={characters} />
+      {filteredData.map(char => {
+        return <CharacterCard key={char.id} character={char} />;
+      })}
     </section>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 export default function WelcomePage() {
   return (
@@ -10,6 +11,7 @@ export default function WelcomePage() {
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
+        <Link className="main-buttons" to={'/characters'}>See Characters</Link>
       </header>
     </section>
   );
