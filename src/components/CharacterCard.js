46cec5ee50b17f73832f2c88e8c01e71175ea41f
jordan <jordan@jordans-MacBook-Air.local>
2019-11-15 15:32:59 +0700

    rendered character list and images

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return(
+    <div>
+      <h1>Character Name:{props.char.name}</h1>
+      <img src={props.char.image} alt="rick and morty image" />
+    </div>
+  );
 }
