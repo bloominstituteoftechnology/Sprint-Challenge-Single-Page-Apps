9d3659bb20009a751d606f0d6ef76259fbabd3e1
jarrodskahill <jarrodskahill@gmail.com>
2019-11-15 12:34:17 -0800

    created a card with some information and mapped data from api through it

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({character}) {
+  const {image, name, species, gender, status, origin, episode} = character;
+  return (
+    <div className="movie-card">
+    <img src={image} />
+    <h2>{name}</h2>
+    <p>Species: {species}</p>
+    <p>Gender: {gender}</p>
+    <p>Status: {status}</p>
+    <p>Episodes featured {episode.length}</p>
+</div>
+  )
 }
