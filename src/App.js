4eb547c13d85dfaea6a0306ac902b1ad27d8c66d
skye <skyesajous@gmail.com>
2019-11-15 13:03:07 -0800

    MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList";
+import SearchForm from "./components/SearchForm";
+import { Route, Link } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
 export default function App() {
   return (
     <main>
+      <SearchForm />
       <Header />
+
+      <Link to="/">Home</Link>
+      <Link to="/characters">Characters</Link>
+
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
+      {/* <Route path="/results/:id" component={Character} /> */}
     </main>
   );
-}
-{
-  setup;
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,44 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
+import CharacterCard from "./CharacterCard";
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [character, setCharacter] = useState([]);
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        console.log(response);
+        setCharacter(response.data.results);
+      })
+      .catch(error => console.log(error));
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>Characters</h2>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+      <Link to="/Search">
+        <button>Search</button>
+      </Link>
+      {character.map(characters => {
+        return (
+          <CharacterCard
+            key={characters.id}
+            name={characters.name}
+            img={characters.image}
+            gender={characters.gender}
+            species={characters.species}
+            status={characters.status}
+          />
+        );
+      })}
     </section>
   );
 }
