2fc5fd811b94a4115f3374bb55bd8c4a0f57c21c
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-28 19:15:38 +0100

    get a list of characters
    In this commit, I made a get request to the rick&morty api to
    get a list of characters. The api result is the set to app state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
+import axios from "axios";
+
+const initialCharacterList = []
+const url = "https://rickandmortyapi.com/api/character/";
 
 
 export default function App() {
+  const [charactersList, setCharactersList] = useState(initialCharacterList)
+
+  useEffect(() => {
+    axios.get(url)
+    .then(res => {
+      setCharactersList(res.data.results);
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
+  }, [])
   return (
     <main>
       <Header />
+      
     </main>
   );
 }
