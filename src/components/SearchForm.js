b281cc9877e51eedf7b369181cb52d9ac8154b80
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-30 12:40:25 +0100

    Add search field to <SearchForm /> component, passing a search() event handler function and the characterList state

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
+  const { search } = props;
  
   return (
     <section className="search-form">
-     // Add a search form here
+     <form>
+       <input onChange={search} type="text" name="search" id="searchInput" />
+     </form>
     </section>
   );
 }
