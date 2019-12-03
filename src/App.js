0cef23dad4d54cb131c328438bc717b788806c47
Temitope Akinsoto <takinsoto@gmail.com>
2019-12-03 22:10:54 +0100

    Render <WelcomePage /> component in <App/> component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React, { useEffect, useState }  from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header.js";
-import CharacterList from './components/CharacterList';
-import SearchForm from './components/SearchForm'
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import SearchForm from "./components/SearchForm";
 import axios from "axios";
 
-
-const initialCharacterList = []
+const initialCharacterList = [];
 const url = "https://rickandmortyapi.com/api/character/";
 
-const search = e => {
-  console.log(e.target.value)
-}
-
-
 export default function App() {
-  const [charactersList, setCharactersList] = useState(initialCharacterList)
+  const [charactersList, setCharactersList] = useState(initialCharacterList);
+
+  const search = e => {
+    const searchedItem = e.target.value;
+    const searched = charactersList.filter(
+      character => character.name === searchedItem
+    );
+    setCharactersList(searched);
+  };
 
   useEffect(() => {
-    axios.get(url)
-    .then(res => {
-      setCharactersList(res.data.results);
-    })
-    .catch(err => {
-      console.log(err.message);
-    })
-  }, [])
-  
+    axios
+      .get(url)
+      .then(res => {
+        setCharactersList(res.data.results);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
-    <main>      
-      <Header />
-      <SearchForm search={search} charactersList={charactersList} />
-      <CharacterList charactersList={charactersList} />      
+    <main>
+      <div>
+        <Header />
+        <SearchForm />
+      </div>
+
+      <WelcomePage />
+      <CharacterList charactersList={charactersList} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,25 +1,19 @@
 import React from "react";
-import CharacterCard from './CharacterCard';
-
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList(props) {
   const { charactersList } = props;
 
   return (
     <section className="character-list">
-      {
-        charactersList.map(character => 
-          (
-            <CharacterCard 
-            key={character.id} 
-            name={character.name}
-            status={character.status}
-            species={character.species}
-            
-            />
-          )
-        )
-      }
+      {charactersList.map(character => (
+        <CharacterCard
+          key={character.id}
+          name={character.name}
+          status={character.status}
+          species={character.species}
+        />
+      ))}
     </section>
   );
 }
