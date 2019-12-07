9a51d96df32a44ed54601c1c0d521abc697cd500
Temitope Akinsoto <takinsoto@gmail.com>
2019-12-07 17:41:38 +0100

    Add css styles to the welcome page using styled components

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+import styles from "styled-components";
+
+const Header = styles.header`
+display: flex;
+flex-direction: column;
+align-items: center;
+img {
+  
+  border-radius: 50%;
+}
+`;
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
-      <header>
-        <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        />
-      </header>
+      <Header>
+        
+          <h1>Welcome to the ultimate fan site!</h1>
+          <Link to='/characters'>
+          <img
+            className="main-img"
+            src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            alt="rick"
+          />
+        </Link>
+      </Header>
     </section>
   );
 }
