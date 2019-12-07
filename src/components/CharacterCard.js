67a1632f9d23b89edbee8a6324127c31ab2c28f7
Temitope Akinsoto <takinsoto@gmail.com>
2019-12-07 14:23:31 +0100

    Add css styles to Character card div

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,6 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "styled-components";
+
+const StyledDiv = styles.div`
+margin: 10px auto;
+height: 100px;
+width: 320px;
+border: 1px solid gray;
+text-align: center;
+background-color: gray;
+text-decoration: none;
+padding-bottom: 20px;
+:hover {
+  background-color: silver;
+  font-color: gray;
+}
+a {
+  text-decoration: none;
+  color: white;
+}
+`;
 
 export default function CharacterCard(props) {
   const { name, status, species } = props;
-return <div>{name} is a {species} and is {status}</div>;
+  return (
+    <StyledDiv>
+      <Link>
+        <p>Name: {name}</p>
+        <p>Species: {species} </p>
+        <p>Status: {status}</p>
+      </Link>
+    </StyledDiv>
+  );
 }
