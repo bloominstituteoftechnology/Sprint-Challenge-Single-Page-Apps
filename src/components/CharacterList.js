bcee02b0061aa83dd14e56c5340e9d16e5c3f26f
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2019-12-14 10:01:35 -0800

    Added axios and mapped over the charactes array

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from "react";
+import axios from'axios'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+const [characters, setCharacters] = useState([]);
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  axios
+  .get('https://rickandmortyapi.com/api/character')
+  .then(response => {
+    console.log("DATA", response.data.results)
+    setCharacters(response.data.results)
+  })
+  .catch(error =>{
+    console.error('Server error', error)
+  })
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(item =>(
+       <div key ={item.id}>
+         <img className="character-image" src={item.image} alt={item.image} />
+          <h1>{item.name}</h1>
+       </div>
+      ))}
     </section>
   );
 }
