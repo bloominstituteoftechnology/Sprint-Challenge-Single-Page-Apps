37a5210f3f8a6fe0b304c4fa2bc5a483e18b8311
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2019-12-14 09:40:06 -0800

    added routes and links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <nav>
+        <div className="linkDiv">
+          <Link to="/">Home Page</Link>
+          <br/>
+          <Link to="/list">Characters</Link>
+        </div>
+      </nav>
     </header>
   );
 }
