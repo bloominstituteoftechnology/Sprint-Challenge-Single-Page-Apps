ab3a078ecf4d420c691c7a6979ba13b493933757
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2019-12-14 12:13:19 -0800

    Changed the welcome page image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import "../index.css";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
       <nav>
-        <div className="linkDiv">
+        <div className="nav-links">
           <Link to="/">Home Page</Link>
           <br/>
           <Link to="/list">Characters</Link>
