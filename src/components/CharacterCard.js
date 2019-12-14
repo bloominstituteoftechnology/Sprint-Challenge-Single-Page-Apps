3667e16122da4f8ebdcaa57109224edc98ded19e
Antoinette <create@aplusmedia.info>
2019-12-13 21:41:27 -0500

    charactercard complete

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import { Col } from 'reactstrap';
+// import CharacterList from "./CharacterList";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+
+
+export default function CharacterCard(props) {
+  
+
+  return (
+    
+    <Col xs="6" sm="4">
+      
+      <Card>
+      
+      <span>{props.name}</span>
+
+              {/* <h4>Name: {props.name}</h4> */}
+              <img src={props.image} alt='rick & morty characters'/>
+              <p>Status: {props.status}</p>
+            
+              <p>Species: {props.species}</p>
+            
+              <p>Gender: {props.gender}</p>
+      </Card>
+    
+    </Col>
+  );
+};
