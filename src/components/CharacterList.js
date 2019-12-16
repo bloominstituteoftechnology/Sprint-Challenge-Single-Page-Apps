8089bb9816aa78e4a32f2d0c22e5bedddc0ea68a
Shyamasri Biswas (Shya) <54689969+shyabiswas@users.noreply.github.com>
2019-12-16 18:48:15 -0500

    Add files via upload

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,89 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [info, setInfo] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+    .get("https://rickandmortyapi.com/api/character/")
+    .then(data => {
+      setInfo(data.data.results);
+    })
+    .catch(error => {console.log("There was an error", error);
+  })
+}, []);
+  console.log(info)
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <div>
+      <section className="character-list">
+        {info.map(char => (
+        <CharacterCard url={char.image} names={char.name} gender={char.gender} local={char.location.name} species={char.species} status={char.status} />
+        ))};
+      </section>
+    </div>
   );
 }
+
+
+
+
+// import React, { useEffect, useState } from "react";
+// import Axios from "axios";
+// import CharacterCard from "./CharacterCard";
+// import SearchForm from "./SearchForm"
+
+
+// function CharacterList() {
+//   // TODO: Add useState to track data from useEffect
+// const [data, setData]= useState([]);
+// const [search, setSearch]=useState('')
+//   useEffect(() => {
+//     Axios
+//     .get(`https://rickandmortyapi.com/api/character/`)
+//     .then (res => {
+//       console.log(res);
+//       const characters= res.data.results;
+//       setData(characters);
+//     })
+//     .catch(err => {
+//       console.log('No data return', err);
+//     })
+//     // TODO: Add API Request here - must run in `useEffect`
+//     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+//   }, []);
+
+//   const handleChange = (event) => {
+//     event.preventDefault();
+//     setSearch(event.target.value)
+//   }
+// const handleSubmit = (event) => {
+//   event.preventDefault();
+  
+// }
+//   return (
+//     <section className="character-list">
+//       <h2></h2>
+//       <SearchForm
+//       placeholder= 'Search characters'
+//       value={search}
+//       handleChange={handleChange}
+//       handleSubmit={handleSubmit}
+//       />
+//       {data.map(character => (
+//         <CharacterCard
+//         key={character.id}
+//         name={character.name}
+//         image={character.image}
+//         species={character.species}
+//         />
+//       ))}
+//     </section>
+//   );
+// }
+
+// export default CharacterList
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function SearchForm() {
+
+  const [characters, setCharacter] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState(characters);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    axios
+    // API Goes in Quotes Behind Get
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(data => {
+        setCharacter(data.data.results);
+      })
+      .catch(error => {console.log("There's an error", error);
+    });
+
+    const results = characters.filter(character => {
+      return character.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    setSearchResults(results);
+
+  }, [searchTerm]);
  
   return (
     <section className="search-form">
-     // Add a search form here
+     <form>
+       <label htmlFor="title">Search:</label>
+       <input id="title" type="text" name="title" onChange={handleChange} 
+       value={searchTerm} />
+     </form>
+     {searchResults.map(char =>(
+       <CharacterCard url={char.image} names={char.name} gender={char.gender} local={char.location.name} specieis={char.species} />
+     ))}
     </section>
   );
 }
