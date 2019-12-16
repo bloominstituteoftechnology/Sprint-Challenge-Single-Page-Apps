d4169c84a0d9ec3b1c84f9c95d5a96e724d8f124
alberto90rodriguez <arodriguez.jr@outlook.com>
2019-12-16 16:09:32 -0600

    passed props to search form and created even handler

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,33 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
-  return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
+function SearchForm(props) {
+  const [result, setResult] = useState();
+  const handleChanges = event => {
+    setResult(event.target.value);
+  };
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+  const filter = props.character.filter(character => {
+    return character.name.indexOf(result) !== -1;
+  });
+  props.searching(filter)
+};
+
+return ( 
+  <section>
+  <form onSubmit={submitHandler}>
+  <input
+    onChange={handleChanges}
+    type='text'
+    name='character'
+    id='character'
+    placeholder='Search'
+  />
+  </form>
+  </section>
+);
 }
+
+export default SearchForm;
