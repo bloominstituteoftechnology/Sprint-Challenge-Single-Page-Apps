de2bb92a73177863ab020e6b4dd316aaf3ffab7c
Aaron Andersen <anderseana@tutanota.com>
2019-12-20 20:21:20 -0600

    working, MVP met

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,37 +1,40 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-const SearchForm = (props) => {
-
+const SearchForm = props => {
   const [character, setCharacter] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
       .get(`https://rickandmortyapi.com/api/character/?name=${search}`)
       .then(response => {
-        console.log(response.data.results)
-        setCharacter(response.data.results)
+        console.log(response.data.results);
+        setCharacter(response.data.results);
       })
       .catch(error => {
-        console.log(error)
-      })
-  }, [search])
-    
-    const handleChange = e => {
-      e.preventDefault(e.target.value);
-      setSearch(e.target.value);
-    };
-    return (
-      <section className="search-form">
-        <form>
-          <input
-            placeholder="Search Your Favorite Character"
-            value={props.search}
-            onChange={handleChange}
-          />
-        </form>
-      </section>
-    )
-  }
+        console.log(error);
+      });
+  }, [search]);
+
+  const handleChange = e => {
+    e.preventDefault(e.target.value);
+    setSearch(e.target.value);
+  };
+  return (
+    <section className="search-form">
+      <form>
+        <input
+          placeholder="Search..."
+          value={props.search}
+          onChange={handleChange}
+        />
+      </form>
+      {character.map((person, index, image) => (
+        <CharacterCard key={index} character={person} image={image} />
+      ))}
+    </section>
+  );
+};
 export default SearchForm;
