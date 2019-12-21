b47e2a49e4fb43376f9a71828a692517e5752b88
R <r@Rs-MacBook-Air.local>
2019-12-20 17:05:36 -0800

    5pm

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,48 +1,94 @@
 import React, { useState, useEffect } from "react";
 import ReactDom from 'react-dom';
+import axios from 'axios'
 
 
-export default function SearchForm(props) {
-  console.log(props)
+// export default function SearchForm(props) {
+//   console.log(props)
 
   
 
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [searchResults, setSearchResults] = useState([]);
 
-  useEffect(() => {
-    const results = props.characters.filter(character =>
+//   useEffect(() => {
+//     const results = props.characters.filter(character =>
       
-      character.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    console.log(results)
-    setSearchResults(results);
-  }, [searchTerm]);
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-  };
+//       character.toLowerCase().includes(searchTerm.toLowerCase())
+//     );
+//     console.log(results)
+//     setSearchResults(results);
+//   }, [searchTerm]);
+//   const handleChange = event => {
+//     setSearchTerm(event.target.value);
+//   };
  
-  return (
-    <section className="search-form">
+//   return (
+//     <section className="search-form">
      
-     <form>
-        <label htmlFor="name">Search:</label>
+//      <form>
+//         <label htmlFor="name">Search:</label>
+//         <input
+//           id="name"
+//           type="text"
+//           name="textfield"
+//           placeholder="Search"
+//           value={searchTerm}
+//           onChange={handleChange}
+//         />
+//       </form>
+//       <div className="character-list">
+//         <ul>
+//           {searchResults.map(character => (
+//             <li key={character}>{character}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     </section>
+//   );
+// }
+export default function Characters(props) {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        'https://rickandmortyapi.com/api/character/',
+        
+      )
+      .then(response => {
+        console.log(response)
+
+        const characters = props.characters.filter(
+          results =>
+            results.name
+              .toLowerCase()
+              .includes(query.toLowerCase())
+        );
+        setData(characters);
+      });
+  }, [query]);
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+  return (
+    <div>
+      <form className="search">
         <input
-          id="name"
           type="text"
-          name="textfield"
-          placeholder="Search"
-          value={searchTerm}
-          onChange={handleChange}
+          onChange={handleInputChange}
+          value={query}
+          name="name"
+          tabIndex="0"
+          className="prompt search-name"
+          placeholder="search by name"
+          autoComplete="off"
         />
       </form>
-      <div className="character-list">
-        <ul>
-          {searchResults.map(character => (
-            <li key={character}>{character}</li>
-          ))}
-        </ul>
-      </div>
-    </section>
+      
+      
+    </div>
   );
 }
+
+
