1e13fd08b99c9d5a48130a64cc394cb5e021bae6
wesley.moody <wgm3190@gmail.com>
2019-12-22 12:24:11 -0600

    use this one

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,37 +1,18 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import  styles from "styled-components";
-
-
-const Buttons = styles.button`
--khtml-box-shadow: 5px 5px 5px #A9A9A9;`
-
-
-const Page = styles.section`
-justify-content:center;`
-
-
-const Img = styles.img`
-margin-top: 5%;`
-
+import {Link} from 'react-router-dom';
 
 export default function WelcomePage() {
   return (
-    <Page className="welcome-page">
-
+    <section className="welcome-page">
       <header>
-        <h1>Welcome to the ultimate fan site!</h1>        
-        <Link to="/Characters" />
-        <Buttons>Characters</Buttons>
-        <Link to="/Search" />
-        <Buttons>Search</Buttons>        
+        <h1>Welcome to the ultimate fan site!</h1>
+        <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
-
+        <Link className="main-buttons" to={'/characters'}>See Characters</Link>
       </header>
-
-    </Page>
+    </section>
   );
 }
