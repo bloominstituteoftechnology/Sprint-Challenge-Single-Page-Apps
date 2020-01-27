a9458c9fe5ecccac9ed1df04484e384c3a86a2ab
Jordan Lanier <jordan.6195@yahoo.com>
2020-01-27 16:59:05 -0500

    changes

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,4 @@
-  
-import React from "react";
+  import React from "react";
 
 export default function CharacterCard() {
   return <span>todo: character</span>;
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+  import React from "react";
 
 export default function LocationCard({ name, type, dimension, residents }) {
   return <span>todo: location</span>;
