a607fe45ea99c241e7e2ff7708b4f99f0360a796
Jordan Lanier <jordan.6195@yahoo.com>
2020-01-27 18:52:05 -0500

    color changes

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Menu, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
+
 export default function TabNav() {
     return (
         <Menu>
