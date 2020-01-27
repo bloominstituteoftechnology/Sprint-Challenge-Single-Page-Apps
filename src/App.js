4bf644b58a8f50db2173dfec179f4c878a8bff15
Jordan Lanier <jordan.6195@yahoo.com>
2020-01-27 15:54:02 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from "react";
-import TabNav from "./components/TabNav.js";
-import Header from "./components/Header.js";
-
+import React from 'react';
+import Header from './components/Header.js';
 
 export default function App() {
-  return (
-    <main>
-      <Header />
-      {/* <TabNav /> */}
-    </main>
-  );
+	return (
+		<main>
+			<Header />
+		</main>
+	);
 }
