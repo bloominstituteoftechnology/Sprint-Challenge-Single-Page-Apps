a896f54e3d772fa8adcd9efbcefdcdf8509cd1c1
Jordan Lanier <jordan.6195@yahoo.com>
2020-01-27 19:14:24 -0500

    done

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from './CharacterCard';
+import axios from 'axios';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [ characters, setCharacters ] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then((res) => {
+        setCharacters(res.data.results);
+      })
   }, []);
 
+  console.log(characters);
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+    <section className="character-list grid-view">
+      {characters.map((char) => {
+        return (
+          <CharacterCard key={char.name} name={char.name} image={char.image} location={char.location} status={char.status} origin={char.origin} />
+        )
+      })}
     </section>
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Menu, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-
 export default function TabNav() {
     return (
         <Menu>
