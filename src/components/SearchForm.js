89122b42bf5966f7f75d16b5d893a0758ade2cd9
Minakshi Verma <mvedwan@gmail.com>
2020-01-31 13:56:53 -0500

    "woking on search"

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -10,12 +10,18 @@ export default function SearchForm(props) {
   const handleChanges = (e) => {
     setQuery(e.target.value)
   }
+
+  const submitForm =e =>{
+    e.preventDefault();
+    setData(props.data)
+
+  }
  
   return (
     <section className="search-form">
      {/* Add a search form here */}
 
-     <form>
+     <form onSubmit = {submitForm}>
        <label htmlFor = "search">Search here</label>
        <input
        id = "search"
@@ -23,7 +29,7 @@ export default function SearchForm(props) {
        name = "search"
        onChange = {handleChanges}
        placeholder = "Type here"
-      //  value = {props.name} 
+       value = {query} 
        />
        <button type ="submit">Search</button>
      </form>
