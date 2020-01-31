371187ee39dfa53996ae63311b7b7c9a3d9ac192
unknown <jrhemann@gmail.com>
2020-01-31 13:20:35 -0600

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList.js";
+import WelcomePage from "./components/WelcomePage.js";
+import { Route } from 'react-router-dom'
+import SearchForm from "./components/SearchForm.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route path ='/Search'><SearchForm /></Route>
+      <Route exact path='/'><WelcomePage /></Route>
+      <Route path = '/Characters'><CharacterList /></Route>
+      
     </main>
   );
 }
