c01c4aae00368dc79b9fa4c90c68dfc68a92b461
ricardo-ljr <ricardo.leitejr95@gmail.com>
2020-01-31 09:50:18 -0700

    Completed MVP

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  const Card = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: Orange;
+    margin: 5% 20%
+    border: solid 5px black;`;
+
+  return (
+    <Card>
+      <h2>{props.name}</h2>
+      <p>{props.species}</p>
+      <p>{props.status}</p>
+      <p>{props.location}</p>
+    </Card>
+  );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,39 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function WelcomePage() {
+  const Page = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const Img = styled.img`
+    margin-top: 10%;
+    margin-left: 15%;
+  `;
+
   return (
-    <section className="welcome-page">
-      <header>
-        <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        />
-      </header>
-    </section>
+    <Page>
+      <section className="welcome-page">
+        <header>
+          <h1>Welcome to the ultimate fan site!</h1>
+          <Link to="/Characters">
+            <button>Characters</button>
+          </Link>
+          <Link to="/Search">
+            <button>Search</button>
+          </Link>
+          <Img
+            className="main-img"
+            src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            alt="rick"
+          />
+        </header>
+      </section>
+    </Page>
   );
 }
