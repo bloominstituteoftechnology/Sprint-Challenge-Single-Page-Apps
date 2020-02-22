524ee080553a537c2c4fe968d04cfc8fa9925173
thomas <t_huynh927@yahoo.com>
2020-02-21 20:37:58 -0800

    did more stretches

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
+import { Pagination } from 'semantic-ui-react';
 
 import SearchForm from "./SearchForm";
 
@@ -15,31 +16,74 @@ const CharacterListHeader = styled.h2`
 
 `
 
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = value => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [ characters, setCharacters ] = useState([]);
   const [ searchResults, setSearchResults ] = useState([])
-  const [ search, setSearch ] = useState("")
+  const [ saveSearch, setSaveSearch ] = useLocalStorage()
+  const [ page, setPage ] = useState([])
+  const [ search, setSearch ] = useState(saveSearch || "")
+  const [ status, setStatus ] = useState("")
+  
   useEffect(() => {
+    const checkStatus = status ? status : "alive"
+
     axios
-      .get(`https://rickandmortyapi.com/api/character/`)
+      .get(`https://rickandmortyapi.com/api/character/?status=${checkStatus}`)
       .then((res) => {
         setCharacters(res.data.results)
         setSearchResults(res.data.results)
+        setPage(res.data.results.slice(0, 5))
       })
       .catch((err) => console.log(err))
-  }, []);
-  console.log(search)
+  }, [status]);
+
   const activateSearch = e => {
     e.preventDefault()
     const results = characters.filter((character) => character.name.toLowerCase().includes(search.toLocaleLowerCase()))
+    setSaveSearch(search)
     setSearchResults(results)
   }
+
+  const handlePageChange = (e=undefined, { activePage } = 1) => {
+    const startingIndex = (activePage ? activePage : 1) - 1
+    const currentPage = searchResults.slice(startingIndex * 5, (startingIndex + 1) * 5)
+    setPage(currentPage)
+  }
+
+  // console.log(characters)
   return (
     <section className="character-list">
       <CharacterListHeader>LIST OF CHARACTERS: </CharacterListHeader>
-      <SearchForm search={search} setSearch={setSearch} activateSearch={activateSearch} />
-      {searchResults.map((character) => <Name key={character.id}>{character.name.toUpperCase()}</Name>)}
+      <SearchForm search={search} setSearch={setSearch} activateSearch={activateSearch} setStatus={setStatus} />
+      {page.map((character) => <Name key={character.id}>{character.name.toUpperCase()}</Name>)}
+      <Pagination onPageChange={handlePageChange} defaultActivePage={1} totalPages={Math.floor(searchResults.length/5)} />
     </section>
   );
 }
+
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 
 
-export default function SearchForm({ search, setSearch, activateSearch }) {
+export default function SearchForm({ search, setSearch, activateSearch, setStatus }) {
   
 
   return (
     <section className="search-form">
       <form onSubmit={e => activateSearch(e)}>
         <input type="text" onChange={e =>setSearch(e.target.value)} value={search} />
+        <select onChange={e => setStatus(e.target.value)}>
+            <option value="alive">Alive</option>
+            <option value="dead">Dead</option>
+            <option value="unknown">Unknown</option>
+        </select>
         <button>Find</button>
       </form>
     </section>
