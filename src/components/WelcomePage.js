7ab36b1b231b0df622482e95047012c37afcbfbb
Dessa Goodlett <53787065+dmhabh1992@users.noreply.github.com>
2020-02-21 21:53:09 -0600

    added stretch

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -13,16 +13,6 @@ export default function WelcomePage() {
               alt="rick"
             />
           </header>
-          <body className="mainInfo">
-          <p className="aboutRM">
-              After having been missing for nearly 20 years, Rick Sanchez suddenly arrives at daughter Beth's 
-              doorstep to move in with her and her family. Although Beth welcomes Rick into her home, her husband, 
-              Jerry, isn't as happy about the family reunion. Jerry is concerned about Rick, a sociopathic scientist, 
-              using the garage as his personal laboratory. In the lab, Rick works on a number of sci-fi gadgets, 
-              some of which could be considered dangerous. But that's not all Rick does that concerns Jerry. 
-              He also goes on adventures across the universe that often involve his grandchildren, Morty and Summer.
-              </p>
-          </body>
         </section>            
       );
     };
