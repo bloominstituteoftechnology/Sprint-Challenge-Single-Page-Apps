c211b876b15153193d63610bf4778a539ed0707c
craig-paiva <craiglpaiva@gmail.com>
2020-02-23 22:56:39 -0500

    Cannot reach MVP due to [HMR] Waiting for update signal from WDS... error

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,21 @@
 import React from "react";
+import {
+  Card, CardText, CardBody,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ people }) {
+  return (
+    <div className="car-box">
+      <Card>
+        <CardBody>
+          <CardTitle>Name: {people.name}</CardTitle>
+          <img width="110%" src={people.image} alt="character-pic" />
+          <CardSubtitle>status: {people.status} </CardSubtitle>
+          <CardText>species: {people.species}</CardText>
+          <CardText>type: {people.type}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  )
 }
