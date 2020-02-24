4d8380b1e4bc3fbfd54ed2547f9b16cb9bec0a98
Steven Waldron <stevenwaldron@Stevens-MBP.home>
2020-02-23 19:35:39 -0500

    having trouble

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
+import WelcomePage from "./components/WelcomePage";
 import Header from "./components/Header.js";
+import CharacterList from './components/CharacterList';
+
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Link to='/CharacterList'>characters</Link>
+      <Link to='/'>home</Link>
+      <Route exact path='/' component={WelcomePage}/>
+      <Route exact path='/CharacterList' component={CharacterList}/>
+      
+      
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  const character = props.characters.find(character => props.match.params.id === `${character.id}` )
+  console.log(character)
+  return (
+    <div>
+      <image src={character.image}/>
+      <h1>{character.name}</h1>
+      <h2>{character.species}</h2>
+      <h2>{character.status}</h2>
+    </div>
+  )
 }
