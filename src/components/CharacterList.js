298fab292a1df0974e3160e13b9ea277c2f7503b
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-02-25 13:47:46 -0800

    Stretch Progress 💪

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,26 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import SearchForm from './SearchForm';
+import Page from './Page';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const uRL = 'https://rickandmortyapi.com/api/character/';
+  const apiUrl = 'https://rickandmortyapi.com/api/character/';
   const [characters, setCharacters] = useState([]);
+
+  const [url, setUrl] = useState(apiUrl);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     axios
-      .get(uRL)
+      .get(url)
       .then(response => {
         setCharacters(response.data.results);
+        setNextUrl(response.data.info.next);
+        setPrevUrl(response.data.info.prev);
+        console.log('Characters', response);
       })
       .catch(error => console.error(error));
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  }, [url]);
   console.log(characters);
   return (
     <div>
       <SearchForm characters={characters} />
+      <Page next={nextUrl} prev={prevUrl} url={setUrl} />
     </div>
   );
 }
