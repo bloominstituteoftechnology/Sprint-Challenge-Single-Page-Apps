8beffb801cd0387e2d7fc8d0d4f91bc4a5ab90b8
AhmedBarre10 <barreahmed600@gmail.com>
2020-02-28 13:56:43 -0500

    Done

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from "react";
+import CharacterList from "./CharacterList";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return(
+    <header>
+      
+    <div><img src = {props.character.image}/></div>
+      <div>
+     <h1>{props.character.name}</h1> 
+      <h2> {props.character.gender}</h2>
+      <h3> {props.character.species}</h3>
+      </div>
+    </header>
+  );
 }
