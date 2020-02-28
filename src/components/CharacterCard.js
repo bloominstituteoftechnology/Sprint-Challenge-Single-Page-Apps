6e177a4da1f8023656ccdb04bf3d64b71c4b8f40
padgettriver <padgettcriver@gmail.com>
2020-02-28 11:22:11 -0500

    API Working. Characters Appearing

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+// export default function CharacterCard() {
+//   return <span>todo: character</span>;
+// }
+
+const CharacterCard = props => {
+  return (
+    <div>
+      <h1>Character</h1>
+      <h2>Name: {props.name}</h2>
+      <h3>Species: {props.species}</h3>
+    </div>
+  )
 }
+
+export default CharacterCard;
