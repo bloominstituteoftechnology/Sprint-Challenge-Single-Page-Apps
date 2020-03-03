9456d523bebc359f25bf225b75c27b7e8e7679ef
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-03 11:15:11 +0100

    upgrade CRA, change yarn -> npm in README

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import Header from "./components/Header.js";
 
-
 export default function App() {
   return (
-    <main>
+    <main data-testid='app'>
       <Header />
     </main>
   );
