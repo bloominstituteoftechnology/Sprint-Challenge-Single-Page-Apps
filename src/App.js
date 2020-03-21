73931b1d65d507f0875a2d264774d3d6eedc5cdf
Altyn Berdiyeva <aberdiyeva@lisaacademy.org>
2020-03-21 14:07:25 -0500

    some code is here and incomplete search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from './components/CharacterList.js';
+// import LocatonCard from './components/CharacterCard.js';
+import SearchForm from './components/SearchForm';
+import WelcomePage from './components/WelcomePage.js';
+import { Route } from 'react-router-dom';
+
 
 export default function App() {
   return (
     <main data-testid='app'>
+      {/* <Link to='/'></Link> */}
       <Header />
+      <Route exact path='/' component={WelcomePage}></Route>
+      <Route Exact path='/character/:id' component={CharacterList}></Route>
+      
+      {/* <LocatonCard /> */}
+      <SearchForm />
     </main>
   );
 }
