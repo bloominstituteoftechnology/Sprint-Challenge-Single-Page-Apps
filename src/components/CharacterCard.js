da71237a6d67844b1a2727c71635cb14724ea7d2
Benedictify <benedictify@gmail.com>
2020-03-22 20:57:24 -0500

    add styled-components to charactercard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from "react";
+import styled from 'styled-components'
+
+const CharP = styled.p`
+  border: 1px darkred solid;
+  border-radius: 0.4rem;
+  padding: 0.2rem;
+  text-align: center;
+`
 
 export default function CharacterCard(props) {
-  return <p>{props.name}</p>;
+  return <CharP>{props.name}</CharP>;
 }
