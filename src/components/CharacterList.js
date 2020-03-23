9c61d5e15a53212b832c3f6cab3c965d7d555910
Benedictify <benedictify@gmail.com>
2020-03-22 19:15:18 -0500

    add useState variables

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [list, setList] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    
   }, []);
 
   return (
