940b0d9b9d3664404d33a40d6fc806c79f4e13d5
Benedictify <benedictify@gmail.com>
2020-03-22 19:56:49 -0500

    add routes for welcomepage at root and character list at /characters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom'
+
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
+import WelcomePage from './components/WelcomePage'
 
 export default function App() {
   return (
     <main data-testid='app'>
       <Header />
-      <CharacterList />
+
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList}/>
     </main>
   );
 }
