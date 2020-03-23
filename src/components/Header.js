a77d3d2186b1b86e9a48dd3e1b1a4852ccfcc5b5
Benedictify <benedictify@gmail.com>
2020-03-22 19:57:02 -0500

    add links to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Route exact path="/" render={() => <Link to="/characters">Characters</Link>} />
+      <Route exact path="/characters" render={() => <Link to="/">Home</Link>} />
     </header>
   );
 }
