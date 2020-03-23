00ba761f1f026a097ba50b27656433f54c59a921
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-03-22 19:03:15 -0700

    fetched data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList.js"
 export default function App() {
   return (
     <main data-testid='app'>
       <Header />
+      <CharacterList />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
-
+import axios from 'axios';
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [state, setCharacterState] = useState({});
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get(`https://rickandmortyapi.com/api/character/`).then(response => {
+      console.log(response);
+      setCharacterState(response.data.results);
+    });
   }, []);
 
   return (
