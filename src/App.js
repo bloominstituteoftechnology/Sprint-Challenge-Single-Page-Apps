1405fa9cc972e2a973e63317f7911d92c61993e1
Benedictify <benedictify@gmail.com>
2020-03-22 19:29:12 -0500

    retrieving data from API successfully, storing in state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList.js";
 
 export default function App() {
   return (
     <main data-testid='app'>
       <Header />
+      <CharacterList />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -7,9 +8,16 @@ export default function CharacterList() {
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        // response.data.results has the list
+        setList(response.data.results)
+      })
+    }, []);
     
-  }, []);
-
+  console.log(list)
+  
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
